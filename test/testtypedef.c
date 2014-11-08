d73aea7dea4609e002891d7a3e6586963b9bd9f7
kevin100 <kevin100@fnx.com>
2014-11-08 15:33:39 +0800

    D xxx

--- test/testtypedef.c
+++ test/testtypedef.c
@@ -18,11 +18,23 @@
 
 #include <stdio.h>
 
-typedef static int INT32;
+//typedef static int INT32;
 
-unsigned INT32 j = 10;
+//unsigned INT32 j = 10;
 
 void main()
 {
+	char a[10] = {"abcdef"};
+
+	printf("%s\n", a);
+
+	int i;
+	for (i = 0, printf("First=%d\n", i);
+				i < 10/*  , printf("Second=%d\n", i)*/;
+				i++, printf("Third=%d\n", i))
+	{
+		printf("Fourth=%d\n", i);
+	}
+
 
 }
