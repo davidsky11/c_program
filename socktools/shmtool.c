8033d3dc12df1afc20b8a2fbb74c5704eeef039c
kevin100 <kevin100@fnx.com>
2014-11-17 17:26:29 +0800

    A semtool.c shmtool.c - M Makefile

---
+++ socktools/shmtool.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  shmtool.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/17/2014 05:10:30 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kevin (###), kevin101@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
